b525c07a64197c85fb83f4b1403b0cc03e9a95cb
Emily <bruner.emilym@gmail.com>
2019-11-15 11:27:34 -0500

    working on characterlist

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import CharacterCard from "./CharacterCard"
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([])
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
+    axios
+      .get("https://rick-api.herokuapp.com/api/")
+      .then((response) => {
+        setCharacter(response.data.results);
+        console.log(response);
+      })
+      .catch(error => {
+        console.log('Server Error', error);
+      });
+
   }, []);
+
+
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <Link to="/"><button>Home</button></Link>
+      {character.map(char => (
+        <CharacterCard key={char.id} name={char.name} species={char.species} status={char.status} />
+      )
+      )}
     </section>
   );
 }
